ab6cf460b848ac64f12d12971555d9df2f1cb125
drzackyll <adams.z.d@gmail.com>
2017-02-16 17:06:40 -0500

    personal website on Heroku

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Resume = () => {
   return (
     <div>
-      Hello.
+      <iframe src="https://drive.google.com/file/d/0B-gG-OR0niasZU9PYVpHYkZGUG8/preview" width="640" height="480"></iframe>
     </div>
   )
 }
