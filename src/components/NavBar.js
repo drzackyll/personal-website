0b86bad948c29754c0d9b9e3d20744caabc12d80
drzackyll <adams.z.d@gmail.com>
2017-02-20 18:47:30 -0500

    Sets up NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const NavBar = () => {
   return (
-    <div>
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-2 col-xs-offset-9"><Link to="/resume">Resume</Link></div>
+      </div>
     </div>
   )
 }
