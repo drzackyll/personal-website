588e9440cdb152728bc44983e838f06301a6afb7
drzackyll <adams.z.d@gmail.com>
2017-03-21 17:16:02 -0400

    stupid

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div style={{textAlign: "center"}}>
-      If you've got a proposal for a project, don't hesitate to let me know at<br />
+      If you've got a proposal for a project, let me know at<br />
       <a href="mailto:adams.z.d@gmail.com">adams.z.d@gmail.com</a>
     </div>
   )
