1abd28eecb6a791b0e9e5bb84fd9a1d6bc25dd82
drzackyll <adams.z.d@gmail.com>
2017-06-20 10:26:08 -0400

    Adds Blog section, fixes router issues

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import { Route, IndexRoute } from 'react-router'
+import { Route, Router, IndexRoute, browserHistory } from 'react-router'
 import App from './components/App'
 import Resume from './components/resume'
 import Projects from './components/projects'
 import Home from './components/home'
+import Blog from './components/blog'
 import Contact from './components/contact'
 
 const Routes = (
-  <Route path="/" component={App}>
-    <IndexRoute component={Home} />
-    <Route path="/resume" component={Resume} />
-    <Route path="/projects" component={Projects} />
-    <Route path="/contact" component={Contact} />
-  </Route>
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="/resume" component={Resume} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/contact" component={Contact} />
+    </Route>
+  </Router>
 )
 
 export default Routes
