b1e87d18a0218878f753e4e7326ccf9a1bf0fad1
drzackyll <adams.z.d@gmail.com>
2017-06-28 12:06:48 -0400

    Slight shift for mobile

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="col-sm-4 col-sm-offset-4">
           <div className="text-center">
             <br />
-            <img src="../../images/zack.jpg" alt="Zack" style={{width: "100%"}} /><br />
+            <img src="../../images/zack.jpg" alt="" style={{width: "100%"}} /><br />
             I'm Zack. I'm a web developer.
           </div>
         </div>
